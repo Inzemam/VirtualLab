04c88c2d9869e23c7cfa6b8d2dd10546af520b14
Ubuntu <ubuntu@ip-172-31-45-122.us-east-2.compute.internal>
2018-02-13 21:18:25 +0000

    Major Update

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname + '/routes'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/public'));
+//Store all JS and CSS in Scripts folder.
+app.use(express.static(__dirname + '/DCDCConverters'));
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/DCDCConverters',function(req,res){
+  res.sendFile(__dirname +'/DCDCConverters/index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.listen(3000);
