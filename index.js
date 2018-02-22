744e24df39c3474138b50e65305b06ebd886fb45
naumanmoiz <naumanmoiz@gmail.com>
2018-02-22 14:32:30 -0600

    probably improve laoding time

--- index.js
+++ index.js
@@ -12,24 +12,4 @@ app.get('/',function(req,res){
   //It will find and locate index.html from View or Scripts
 });
 
-// app.get('/lectures',function(req,res){
-//   res.render('content', 'hello');
-//   //It will find and locate index.html from View or Scripts
-// });
-
-// var lecture = require('./models/lecture');
-//
-// // view engine setup
-// app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'head', layoutsDir: __dirname + '/views'}));
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-//
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/lectures', lecture);
-
 app.listen(process.env.PORT || 3000);
